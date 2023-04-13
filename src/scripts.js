9371c1a81e3c6e13125a4e2aea4cbcc4c6ce948f
Emiliano C <emiliano.cereceres@student.cart.org>
2023-04-12 21:24:41 -0700

    mesh movement
    sphere now moves depending on how far scrolled the user is

--- src/scripts.js
+++ src/scripts.js
@@ -2,24 +2,27 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
 
 // renderer
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.render(scene, camera);
 
 // controls
 const controls = new OrbitControls( camera, renderer.domElement );
 
-camera.position.y = 5;
-
 // light 
 const light = new THREE.PointLight();
+light.position.y = 10;
 scene.add(light);
+
+// light helper
 const lightHelper = new THREE.PointLightHelper(light);
 scene.add(lightHelper)
-light.position.y = 10;
 
 // box
 const boxGeo = new THREE.BoxGeometry( 25, 25, 25 );
@@ -31,13 +34,21 @@ const box = new THREE.Mesh( boxGeo, boxMat );
 scene.add( box );
 
 // cube 
-const cubeGeo = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMat = new THREE.MeshPhongMaterial( { 
-    color: 0xffffff,
+const cubeGeo = new THREE.SphereGeometry( );
+const cubeMat = new THREE.MeshBasicMaterial( { 
+    color: 0xff00ff,
     // side: THREE.DoubleSide,
 } );
 const cube = new THREE.Mesh( cubeGeo, cubeMat );
 scene.add( cube );
+
+
+
+
+document.getElementById("test").addEventListener("click", () => {
+    console.log(document.body.getBoundingClientRect().top * -0.001)
+    cube.position.y = document.body.getBoundingClientRect().top * -0.001
+})
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -47,5 +58,11 @@ function animate() {
 
 	renderer.render( scene, camera );
 }
-
 animate()
+
+window.addEventListener("resize", windowResize);
+function windowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
