d1890328e8bfa7801200c7327996322f4b2385f3
Emiliano C <emiliano.cereceres@student.cart.org>
2023-04-13 10:42:57 -0700

    ball change position on scroll
    random color for box and sphere every time page loads

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.y = 1;
 
 // renderer
 const canvas = document.querySelector(".webgl");
@@ -25,36 +25,45 @@ const lightHelper = new THREE.PointLightHelper(light);
 scene.add(lightHelper)
 
 // box
-const boxGeo = new THREE.BoxGeometry( 25, 25, 25 );
+const boxGeo = new THREE.BoxGeometry( 25, 100, 25 );
 const boxMat = new THREE.MeshPhongMaterial( { 
-    color: 0xffffff,
+    color: 0x000000,
     side: THREE.DoubleSide,
 } );
 const box = new THREE.Mesh( boxGeo, boxMat );
 scene.add( box );
 
-// cube 
-const cubeGeo = new THREE.SphereGeometry( );
-const cubeMat = new THREE.MeshBasicMaterial( { 
-    color: 0xff00ff,
-    // side: THREE.DoubleSide,
+// sphere 
+const sphereGeo = new THREE.SphereGeometry(1, 64, 64 );
+const sphereMat = new THREE.MeshBasicMaterial( { 
+    side: THREE.DoubleSide,
 } );
-const cube = new THREE.Mesh( cubeGeo, cubeMat );
-scene.add( cube );
+const sphere = new THREE.Mesh( sphereGeo, sphereMat );
+scene.add( sphere );
 
-
+let colors = ["b7999c", "8f9e87", "638b83", "c7b57d", "b4a9c7", "e1c9b1", "7a8b99", "d6a8b6", "bfb8a5", "9b9e77"]
+let darkColors = ["4f6b65", "8d7e9e", "4bb9f2", "87B4C7", "B1C79F", "b67554", "3f4e6d", "8d4c5e", "817569", "4f524a"]
+let ranNum = Math.floor(Math.random() * 3)
+console.log(colors[ranNum], darkColors[ranNum])
+sphere.material.color.setHex( `0x${colors[4]}` )
+box.material.color.setHex( `0x${darkColors[4]}`)
 
 
 document.getElementById("test").addEventListener("click", () => {
     console.log(document.body.getBoundingClientRect().top * -0.001)
-    cube.position.y = document.body.getBoundingClientRect().top * -0.001
+    sphere.position.y = document.body.getBoundingClientRect().top * -0.001
+})
+
+document.addEventListener("scroll", () => {
+    // console.log(document.body.getBoundingClientRect().top * 0.01)
+    sphere.position.y = document.body.getBoundingClientRect().top * 0.01
 })
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
