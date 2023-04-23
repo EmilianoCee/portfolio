9a14f83ebabb9d6cbb4f4fe9c214c0cec335587f
Emiliano C <emiliano.cereceres@student.cart.org>
2023-04-23 16:26:13 -0700

    cloned cactus object
    second cactus in place, first cactus has shadow

--- src/index-3d.js
+++ src/index-3d.js
@@ -15,6 +15,8 @@ const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render(scene, camera);
 
+renderer.shadowMap.enabled = true;
+
 // controls
 const controls = new OrbitControls( camera, renderer.domElement );
 
@@ -22,34 +24,52 @@ const controls = new OrbitControls( camera, renderer.domElement );
 
 // light 
 const light = new THREE.PointLight();
-light.position.set(5, 15, 5)
+light.position.set(0, 10, 10)
+light.castShadow = true;
 scene.add(light);
 
 // light helper
 const lightHelper = new THREE.PointLightHelper(light);
 scene.add(lightHelper)
 
-let colors = ["b7999c", "8f9e87", "638b83", "c7b57d", "b4a9c7", "e1c9b1", "7a8b99", "d6a8b6", "bfb8a5", "9b9e77"]
-let darkColors = ["4f6b65", "8d7e9e", "8C7456", "87B4C7", "B1C79F", "84948F", "99906B", "5F8A7C", "9BB6BF", "87869E"]
-let ranNum = Math.floor(Math.random() * colors.length)
+// let colors = ["b7999c", "8f9e87", "638b83", "c7b57d", "b4a9c7", "e1c9b1", "7a8b99", "d6a8b6", "bfb8a5", "9b9e77"]
+// let darkColors = ["4f6b65", "8d7e9e", "8C7456", "87B4C7", "B1C79F", "84948F", "99906B", "5F8A7C", "9BB6BF", "87869E"]
+// let ranNum = Math.floor(Math.random() * colors.length)
 
 const assetLoader = new GLTFLoader();
 
+// box
+const boxGeo = new THREE.BoxGeometry(150,15,150);
+const boxMat = new THREE.MeshPhongMaterial( {
+    side: THREE.DoubleSide,
+    shininess: false,
+});
+const box = new THREE.Mesh(boxGeo, boxMat);
+box.receiveShadow = true;
+box.position.set(0,7.5,0)
+scene.add(box)
+
 // cactus object
 let cactus;
+let cactus2;
 assetLoader.load(`/src/assets/cactus.glb`, function(gltf) {
     cactus = gltf.scene;
     // cactus.scale.set(15,15,15);
     scene.add(cactus);
-
+    
+    cactus2 = cactus.clone();
+    cactus2.position.set(10, 0, 0);
+    scene.add(cactus2);
+    
     cactus.traverse(function (child) {
         if (child.isMesh) {
-          child.material.color.setHex( `0x${colors[ranNum]}` )
+        //   child.material.color.setHex( `0x${colors[ranNum]}` )
+            child.castShadow = true;
         }
     });
 
 }, function (xhr) {
-    console.log(`last name ${(xhr.loaded / xhr.total * 100)} % loaded`);
+    console.log(`cactus ${(xhr.loaded / xhr.total * 100)} % loaded`);
 }, undefined, function(error) {
     console.error(error)
 })
@@ -57,19 +77,18 @@ assetLoader.load(`/src/assets/cactus.glb`, function(gltf) {
 // random torus' in background
 // const torusGeo = new THREE.TorusGeometry();
 // const torusMat = new THREE.MeshBasicMaterial();
-// // const torus = new THREE.Mesh(torusGeo, torusMat);
+// const torus = new THREE.Mesh(torusGeo, torusMat);
 // function addTorus() {
-//     const torus = new THREE.Mesh( torusGeo, torusMat);
+//     // const torus = new THREE.Mesh( torusGeo, torusMat);
 //     for ( let i = 0; i < 100; i ++ ) {
-//         torus.position.x = Math.random() * 300 - 150;
+//         cactus.position.x = Math.random() * 300 - 150;
 //         // torus.position.y = -Math.random() * 50 - 100;
-//         torus.position.y = -100
-//         torus.position.z = Math.random() * 200 - 100;
+//         cactus.position.y = -100
+//         cactus.position.z = Math.random() * 200 - 100;
 
-//         torus.rotation.set(Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI)
-//         torus.material.color.setHex( `0x${colors[ranNum]}` )
+//         cactus.rotation.set(Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI)
 
-//         scene.add( torus );
+//         scene.add( cactus );
 //     }
 // }
 // Array(150).fill().forEach(addTorus);
